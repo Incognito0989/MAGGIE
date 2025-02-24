36fc8399f1856a58f862cd67e778e45ad3ebf2a7
Incognito0989 <andrew109jones@gmail.com>
2025-02-24 13:37:37 -0500

    update settings to be same as SIS... Change gui user to be  Admin/Password for customer

--- settings.py
+++ settings.py
@@ -1,15 +1,17 @@
 ## GLOBAL VARIABLES
 management_port = 48            #this is physical port 47
-switch_ip = "10.4.11.240"
+switch_ip = "192.168.2.240"
 switch_username = "netadmin"  # Replace with your username
 switch_password = "Syn@123!!"  # Replace with your SSH login password
 switch_enable_password = "Syna1234"  # Replace with your enable password
 
-meg_ip = "10.4.11.68"
+meg_ip = "192.168.2.20"
 meg_username="root"
 meg_password="$ynamedia"
-meg_rest_username="maggie"
-meg_rest_password="maggie"
+meg_rest_username="Admin"
+meg_rest_password="Password"
 
-port_exclusions = [18, 34, management_port]
+port_exclusions = [management_port]
 ## port range that is being used is  2 ... 49
+
+pub_key = ""
